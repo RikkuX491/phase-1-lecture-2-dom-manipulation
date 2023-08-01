1abeafc5856b7535ea5645aef0023766d88ba7a8
Ricardo Guerra <RikkuG491@gmail.com>
2023-08-01 18:03:01 -0400

    Solution code from yesterday's lecture

--- src/index.js
+++ src/index.js
@@ -53,3 +53,58 @@ const otherFoods = [
         description: "The Flatburger exclusive Onion Rings! Get them while supplies last!"
     }
 ]
+
+const restaurantMenu = document.getElementById('restaurant-menu')
+
+// console.log("The boolean equivalent of undefined is: ")
+// console.log(!!undefined)
+
+// Add a burger to the menu
+function addFoodImageToMenu(food, isBurger){
+    // console.log(isBurger)
+    const foodImage = document.createElement('img')
+    foodImage.src = food.image
+    restaurantMenu.appendChild(foodImage)
+
+    if(isBurger){
+        foodImage.className = "burger"
+    }
+}
+
+// for(let index = 0; index < burgers.length; index++){
+//     addFoodImageToMenu(burgers[index])
+// }
+burgers.forEach(burger => {
+    addFoodImageToMenu(burger, true)
+})
+
+// for(let index = 0; index < otherFoods.length; index++){
+//     addFoodImageToMenu(otherFoods[index])
+// }
+otherFoods.forEach(food => {
+    addFoodImageToMenu(food, false)
+})
+
+// const foodDetailImage = document.getElementsByClassName('detail-image')[0]
+const foodDetailImage = document.querySelector('img.detail-image')
+foodDetailImage.src = burgers[0].image
+
+const foodName = document.getElementsByClassName('name')[0]
+foodName.textContent = burgers[0].name
+
+const foodDescriptionDisplay = document.getElementById('description-display')
+foodDescriptionDisplay.textContent = burgers[0].description
+
+const foodImages = document.querySelectorAll('#restaurant-menu img')
+foodImages.forEach(foodImage => {
+    foodImage.style = "border-style: solid; border-color: red; border-width: 3px"
+})
+
+const burgerImages = document.getElementsByClassName('burger')
+
+for(let index = 0; index < burgerImages.length; index++){
+    burgerImages[index].style.borderColor = "blue"
+}
+
+const elementToRemove = document.querySelector('#restaurant-menu img')
+elementToRemove.remove()
